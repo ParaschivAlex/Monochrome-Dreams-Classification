20c9b3261ca46fa8934e963b7d6e73ee0b729610
Paraschiv Alexandru Andrei <56772112+ParaschivAlex@users.noreply.github.com>
2023-06-07 15:19:40 +0300

    Update Monochrome TF.py

--- Monochrome TF.py
+++ Monochrome TF.py
@@ -153,16 +153,16 @@ print(validare)
 #cand am obtinut o solutie peste 0.90 am vrut sa antrenez modelul pentru datele de train + validare pentru a avea o solutie si mai buna dar nu mai aveam submisii
 sample_sub = cnn.predict_classes(load_reshaped_test)
 
-def confusion_matrix(label_true, label_predicted):  # aici afisez matricea de confuzie
+def prediction_matrix(label_true, label_predicted):  # aici afisez matricea de predictii
     num_classes = max(max(label_true), max(label_predicted)) + 1  # iau numarul de clase posibile, puteam sa ii dau 9 eu
-    conf_matrix = np.zeros((num_classes, num_classes))  # face o matrice 9x9 initializata cu 0
+    pred_matrix = np.zeros((num_classes, num_classes))  # face o matrice 9x9 initializata cu 0
 
     for i in range(len(label_true)):  # iau i = numarul de labeluri date (de imagini)
-        conf_matrix[int(label_true[i]), int(label_predicted[i])] += 1  # daca prezicerea este corecta crestem valoarea pe diagonala principala, daca nu in afara ei ( [i][j] i -> ce trebuia prezis si j-> ce a prezis)
-    return conf_matrix
+        pred_matrix[int(label_true[i]), int(label_predicted[i])] += 1  # daca prezicerea este corecta crestem valoarea pe diagonala principala, daca nu in afara ei ( [i][j] i -> ce trebuia prezis si j-> ce a prezis)
+    return pred_matrix
 
 
-print(confusion_matrix(label_validation, validare))
+print(prediction_matrix(label_validation, validare))
 
 g = open("sample_submission.txt", "w")
 print(len(sample_sub), len(test_name_files))
